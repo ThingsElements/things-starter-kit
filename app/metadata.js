3366aaab4a6c24ba0e05fbfb8bd90e63d53688c3
Lyonghwan Park <longhuan@hatiolab.com>
2016-05-02 13:23:18 +0900

    change start kit config

--- app/metadata.js
+++ app/metadata.js
@@ -1,16 +1,16 @@
 var config = require('../config');
 
 module.exports = {
-  title: 'Polymer Starter Kit +',
-  description: 'Polymer Starter Kit Plus',
+  title: 'Things Starter Kit',
+  description: 'Things Polymer Starter Kit Plus',
   charset: 'UTF-8',
-  lang: 'en',
+  lang: 'en-US',
   // Add to homescreen for Chrome on Android
   applicationName: 'PSK+',
   // Add to homescreen for Safari on iOS
   appleMobileWebAppTitle: 'PSK+',
   // Google Analytics Tracking ID
-  googleAnalyticsTrackingId: 'UA-XXXXX-Y',
+  // googleAnalyticsTrackingId: 'UA-XXXXX-Y',
   // App name
   appName: config.appName,
   // App theme
