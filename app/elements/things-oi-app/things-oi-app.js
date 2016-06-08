8df61ae73f078ef5f2e7ccdd7e3862045ac0e772
박용환 <longhuan@hatiolab.com>
2016-06-08 18:10:27 +0900

    change oi interface

--- app/elements/things-oi-app/things-oi-app.js
+++ app/elements/things-oi-app/things-oi-app.js
@@ -5,7 +5,24 @@ window.Things.DataGlobal.inv = window.Things.DataGlobal.inv || {};
 
 Polymer({
     is: 'things-oi-app',
+    properties:{
+    	selectedMenu : {
+    		type: Object
+    	}
+    },
     behaviors:[
-      Things.GlobalBehavior
-    ]
+    	Things.GlobalBehavior,
+		Things.FullscreenBehavior
+    ],
+    ready:function(){
+    	document.addEventListener('things-routing-changed', function(e) {
+          this.selectedMenu = e.detail;
+        }.bind(this));
+    },
+    _fullScreenTap: function () {
+    	this.$.informationContainer.opened = !this.$.informationContainer.opened;
+    	this.target = this.$.contentArea;
+    	this.toggleFullscreen();
+    }
+
 });
